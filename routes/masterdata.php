1f2eb268cfa57ee0830f51989e7c6552de386739
rifqyar <rifqyaditya55@gmail.com>
2024-08-02 18:48:47 +0700

    asdasd

--- routes/masterdata.php
+++ routes/masterdata.php
@@ -4,24 +4,24 @@ use App\Http\Controllers\Module\MasterData\ClientController;
 use App\Http\Controllers\Module\MasterData\MeetingScheduleController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth', 'ajax'])->group(function(){
+Route{
     // Client
     Route::prefix('client')->group(function(){
-        Route::get('/', [ClientController::class, 'index'])->name('masterdata.client')->middleware('can:Client');
-        Route::get('/add', [ClientController::class, 'create'])->name('client.add')->middleware('can:client.add');
-        Route::post('/store', [ClientController::class, 'store'])->name('client.store')->middleware('can:client.add');
+        Route::get('/', [ClientController::class, 'index'])->name('masterdata.client');
+        Route::get('/add', [ClientController::class, 'create'])->name('client.add');
+        Route::post('/store', [ClientController::class, 'store'])->name('client.store');
         Route::post('/data', [ClientController::class, 'data'])->name('client.data');
-        Route::get('/edit/{id}', [ClientController::class, 'edit'])->name('client.edit')->middleware('can:client.edit');
+        Route::get('/edit/{id}', [ClientController::class, 'edit'])->name('client.edit');
         Route::get('/get-detail/{code}', [ClientController::class, 'getDetail']);
     });
 
     // Meeting Schedule
     Route::prefix('meeting-schedule')->group(function(){
-        Route::get('/', [MeetingScheduleController::class, 'index'])->name('masterdata.meeting-schedule')->middleware('can:Meeting Schedule');
-        Route::get('/add', [MeetingScheduleController::class, 'create'])->name('meeting-schedule.add')->middleware('can:meeting.add');
-        Route::post('/store', [MeetingScheduleController::class, 'store'])->name('meeting-schedule.store')->middleware('can:meeting.add');
+        Route::get('/', [MeetingScheduleController::class, 'index'])->name('masterdata.meeting-schedule');
+        Route::get('/add', [MeetingScheduleController::class, 'create'])->name('meeting-schedule.add');
+        Route::post('/store', [MeetingScheduleController::class, 'store'])->name('meeting-schedule.store');
         Route::post('/data', [MeetingScheduleController::class, 'data'])->name('meeting-schedule.data');
-        Route::get('/edit/{id}', [MeetingScheduleController::class, 'edit'])->name('meeting-schedule.edit')->middleware('can:meeting.edit');
+        Route::get('/edit/{id}', [MeetingScheduleController::class, 'edit'])->name('meeting-schedule.edit');
         Route::get('/generate-qr/{id}', [MeetingScheduleController::class, 'generateQrCode'])->name('meeting-schedule.generate-qr');
         Route::get('/get-qr/{id}', [MeetingScheduleController::class, 'getQR'])->name('meeting-schedule.get-qr');
     });
--- routes/transaction.php
+++ routes/transaction.php
@@ -7,11 +7,11 @@ Route::post('/meeting-attendance/store', [MeetingAttendanceController::class, 's
 Route::middleware(['auth', 'ajax'])->group(function(){
     // Meeting Attendance
     Route::prefix('meeting-attendance')->group(function(){
-        Route::get('/', [MeetingAttendanceController::class, 'index'])->name('masterdata.meeting-attendance')->middleware('can:Meeting Attendance');
-        Route::get('/add', [MeetingAttendanceController::class, 'create'])->name('meeting-attendance.add')->middleware('can:meeting.add');
+        Route::get('/', [MeetingAttendanceController::class, 'index'])->name('masterdata.meeting-attendance');
+        Route::get('/add', [MeetingAttendanceController::class, 'create'])->name('meeting-attendance.add');
         // Route::post('/store', [MeetingAttendanceController::class, 'store'])->name('meeting-attendance.store');
         Route::post('/data', [MeetingAttendanceController::class, 'data'])->name('meeting-attendance.data');
-        Route::get('/data/{id}', [MeetingAttendanceController::class, 'edit'])->name('meeting-attendance.edit')->middleware('can:meeting.edit');
+        Route::get('/data/{id}', [MeetingAttendanceController::class, 'edit'])->name('meeting-attendance.edit');
     });
 });
 ?>
